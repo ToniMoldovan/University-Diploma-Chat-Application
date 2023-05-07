2665275b59438adae3e517a911af07ee00b51a92
ToniMoldovan <antonio.moldovan82@yahoo.com>
2023-05-07 16:27:52 +0300

    added server.php

--- websocket-server/server.php
+++ websocket-server/server.php
@@ -1,1 +1,20 @@
 <?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\Chat;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    6062,
+    '0.0.0.0'
+);
+
+
+$server->run();
