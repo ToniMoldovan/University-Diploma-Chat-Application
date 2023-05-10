86f9766644480d5a18412887f32fef96769f443c
ToniMoldovan <antonio.moldovan82@yahoo.com>
2023-05-11 01:20:38 +0300

    added basic methods for routing

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,9 +1,27 @@
 <?php
 
-//add a namespace
 namespace app\controllers;
 
 class HomeController
 {
+    public function index()
+    {
+        require __DIR__ . '/../views/home.php';
+    }
 
+    public function about()
+    {
+        echo 'This is the about page.';
+    }
+
+    public function contact()
+    {
+        echo 'This is the contact page.';
+    }
+
+    //TODO: Create a 404 page
+    public function notFound()
+    {
+        echo '404 - Page not found.';
+    }
 }
