440342c809881e1271115eb69ad82d344082f320
ToniMoldovan <antonio.moldovan82@yahoo.com>
2023-05-11 01:19:00 +0300

    created & styled navbar.php

---
+++ app/views/components/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand" href="#"><img width="48px" src="images/logo.png" alt="Logo"> MyChat</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Chatrooms</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Logout</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
