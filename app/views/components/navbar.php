f8e233f2fb537441fdcb240809916cb84f72ad81
ToniMoldovan <antonio.moldovan82@yahoo.com>
2023-05-18 23:31:17 +0300

    changed navbar design. !not final version!

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -1,24 +1,28 @@
+<!-- Navbar -->
 <nav class="navbar navbar-expand-lg">
-    <a class="navbar-brand" href="#"><img width="48px" src="images/logo.png" alt="Logo"> MyChat</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">Chat App</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="#">Chatrooms</a>
+                <a class="nav-link" href="#">Rooms</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Logout</a>
+                <a class="nav-link" href="#">New Room</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Register</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
+                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img class="profile-picture" src="profile.jpg" alt="Profile Picture">
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="profileDropdown">
+                    <a class="dropdown-item" href="#">My Rooms</a>
+                    <a class="dropdown-item" href="#">Edit Profile</a>
+                    <a class="dropdown-item" href="/logout-user">Logout</a>
+                </div>
             </li>
         </ul>
     </div>
