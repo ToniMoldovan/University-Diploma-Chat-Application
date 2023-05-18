90ca97c5ed674e6c33de33e793ed97ad18cdef40
ToniMoldovan <antonio.moldovan82@yahoo.com>
2023-05-18 23:31:39 +0300

    added routes for AuthController

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use app\controllers\AuthController;
 use app\controllers\HomeController;
 use Klein\Klein;
 
@@ -9,9 +10,15 @@ $router = new Klein();
 
 /* Home routes */
 $router->respond('GET', '/', [new HomeController(), 'index']);
+$router->respond('GET', '/home', [new HomeController(), 'home']);
 $router->respond('GET', '/about', [new HomeController(), 'about']);
 $router->respond('GET', '/contact', [new HomeController(), 'contact']);
 $router->respond('GET', '/404', [new HomeController(), 'notFound']);
+
+/* AuthController routes */
+$router->respond('POST', '/register-user', [new AuthController(), 'register']);
+$router->respond('POST', '/login-user', [new AuthController(), 'login']);
+$router->respond('GET', '/logout-user', [new AuthController(), 'logout']);
 
 // route for 404 REDIRECT to a 404 page
 $router->onHttpError(function ($code, $router) {
