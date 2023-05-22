3b50760f44c101a646e90ee6c61b5762062cb4bd
ToniMoldovan <antonio.moldovan82@yahoo.com>
2023-05-22 18:22:31 +0300

    introduced global variable `assets` for easier use of paths

--- public/index.php
+++ public/index.php
@@ -3,5 +3,7 @@ session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+$GLOBALS['assets'] = '/../';
+
 require_once __DIR__ . '/../app/database.php';
 require_once __DIR__ . '/../app/routes.php';
